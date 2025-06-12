6c3b5b4235ad8683609b4ee2a8c873f7c23a2f89
UNGGU0704 <kyuhung1001@gmail.com>
2025-06-12 15:22:05 +0900

    update: review jpa 스캔 방법 변경

--- review/src/main/java/com/ktds/hi/review/infra/config/ReviewConfig.java
+++ review/src/main/java/com/ktds/hi/review/infra/config/ReviewConfig.java
@@ -1,5 +1,6 @@
 package com.ktds.hi.review.infra.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -8,5 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @Configuration
 @EnableJpaRepositories(basePackages = "com.ktds.hi.review.infra.gateway.repository")
+@EntityScan(basePackages = "com.ktds.hi.review.infra.gateway.entity")
 public class ReviewConfig {
 }
